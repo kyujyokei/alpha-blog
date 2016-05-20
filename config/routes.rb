f1fdab43e405ddf33ef55e339c130164f180d359
Kejo Hsieh <kyujyukei@gmail.com>
2016-05-20 10:43:04 +0000

    Create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get 'about', to:'pages#about'
+  
+  resources :articles # gave a new article path to do the CRUD 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
